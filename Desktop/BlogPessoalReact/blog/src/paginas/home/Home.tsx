f16a70f7ec486a6fd06897e4a8d747820b8cc97b
Samara Pereira <samarapds26@gmail.com>
2022-09-08 14:34:01 -0300

    @isamarx [Task: Criando componente de postagens e temas]{08/09/2022}

---
+++ Desktop/BlogPessoalReact/blog/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css'
+
+function Home(): JSX.Element{
+    return(
+        <div>
+            <h1>Home</h1>
+            <img src="farm.jpg" alt="Imagem Tela Inicial" />
+        </div>
+    );
+}
+
+export default Home;
